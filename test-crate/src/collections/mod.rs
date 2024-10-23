e882f6da3f3519e4d559b068e08ff93f91c2222c
Artem Agvanian <artemagvanian@icloud.com>
2024-10-22 21:57:44 -0400

    Move over the unit test suite from Scrutinizer

---
+++ test-crate/src/collections/mod.rs
@@ -0,0 +1,5 @@
+pub mod btreemap;
+pub mod hashmap;
+pub mod hashset;
+pub mod linked_list;
+pub mod vec;
---
+++ test-crate/src/lib.rs
@@ -0,0 +1,40 @@
+#![feature(allocator_api)]
+#![feature(const_trait_impl)]
+#![feature(const_refs_to_cell)]
+#![allow(unused_variables)]
+
+pub mod collections;
+pub mod tests;
+
+macro_rules! redefine {
+    (<$origin_ty:ty> :: $func_ident:ident => $($param_ident:ident : $param_ty:ty),* => $ret_ty:ty, $purity:expr) => {
+        #[doc = $purity]
+        pub fn $func_ident($($param_ident : $param_ty),*) -> $ret_ty {
+            <$origin_ty>::$func_ident($($param_ident),*)
+        }
+    };
+    (<$origin_ty:ty> :: $func_ident:ident<$($lt:lifetime),+> => $($param_ident:ident : $param_ty:ty),* => $ret_ty:ty, $purity:expr) => {
+        #[doc = $purity]
+        pub fn $func_ident<$($lt),*>($($param_ident : $param_ty),*) -> $ret_ty {
+            <$origin_ty>::$func_ident($($param_ident),*)
+        }
+    };
+    ($new_ident:ident, <$origin_ty:ty> :: $func_ident:ident => $($param_ident:ident : $param_ty:ty),* => $ret_ty:ty, $purity:expr) => {
+        #[doc = $purity]
+        pub fn $new_ident($($param_ident : $param_ty),*) -> $ret_ty {
+            <$origin_ty>::$func_ident($($param_ident),*)
+        }
+    };
+    ($new_ident:ident, <$origin_ty:ty> :: $func_ident:ident<$($lt:lifetime),+> => $($param_ident:ident : $param_ty:ty),* => $ret_ty:ty, $purity:expr) => {
+        #[doc = $purity]
+        pub fn $new_ident<$($lt),*>($($param_ident : $param_ty),*) -> $ret_ty {
+            <$origin_ty>::$func_ident($($param_ident),*)
+        }
+    };
+}
+
+pub(crate) use redefine;
+
+pub fn foo() {
+    println!("Foo");
+}
