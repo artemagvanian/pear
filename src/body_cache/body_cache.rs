2d35364068dd54c0e3e2dad2de95e489243a77f5
Artem Agvanian <artemagvanian@icloud.com>
2024-11-24 17:57:00 -0500

    Adapt cross-crate body caching from Paralegal

---
+++ src/body_cache/body_cache.rs
@@ -0,0 +1,176 @@
+use std::path::PathBuf;
+
+use rustc_hir::{
+    def_id::{CrateNum, DefId, LocalDefId, LOCAL_CRATE},
+    intravisit::{self},
+};
+use rustc_macros::{TyDecodable, TyEncodable};
+use rustc_middle::{
+    hir::nested_filter::OnlyBodies,
+    mir::{Body, ClearCrossCrate, StatementKind},
+    ty::TyCtxt,
+};
+
+use rustc_utils::mir::borrowck_facts::get_body_with_borrowck_facts;
+
+use crate::body_cache::encoder::{decode_from_file, encode_to_file};
+
+/// A mir [`Body`] and all the additional borrow checking facts that our
+/// points-to analysis needs.
+#[derive(TyDecodable, TyEncodable, Debug)]
+pub struct CachedBody<'tcx> {
+    body: Body<'tcx>,
+}
+
+impl<'tcx> CachedBody<'tcx> {
+    /// Retrieve a body and the necessary facts for a local item.
+    ///
+    /// Ensure this is called early enough in the compiler so that the body has not been stolen yet.
+    fn retrieve(tcx: TyCtxt<'tcx>, local_def_id: LocalDefId) -> Self {
+        let body_with_facts = get_body_with_borrowck_facts(tcx, local_def_id);
+        let mut body = body_with_facts.body.clone();
+        clean_undecodable_data_from_body(&mut body);
+
+        Self { body }
+    }
+
+    pub fn owned_body(self) -> Body<'tcx> {
+        self.body
+    }
+}
+
+/// A visitor to collect all bodies in the crate and write them to disk.
+struct DumpingVisitor<'tcx> {
+    tcx: TyCtxt<'tcx>,
+    target_dir: PathBuf,
+}
+
+/// Some data in a [Body] is not cross-crate compatible. Usually because it
+/// involves storing a [LocalDefId]. This function makes sure to sanitize those
+/// out.
+fn clean_undecodable_data_from_body(body: &mut Body) {
+    for scope in body.source_scopes.iter_mut() {
+        scope.local_data = ClearCrossCrate::Clear;
+    }
+
+    for stmt in body
+        .basic_blocks_mut()
+        .iter_mut()
+        .flat_map(|bb| bb.statements.iter_mut())
+    {
+        if matches!(stmt.kind, StatementKind::FakeRead(_)) {
+            stmt.make_nop()
+        }
+    }
+}
+
+impl<'tcx> intravisit::Visitor<'tcx> for DumpingVisitor<'tcx> {
+    type NestedFilter = OnlyBodies;
+    fn nested_visit_map(&mut self) -> Self::Map {
+        self.tcx.hir()
+    }
+
+    fn visit_fn(
+        &mut self,
+        function_kind: intravisit::FnKind<'tcx>,
+        function_declaration: &'tcx rustc_hir::FnDecl<'tcx>,
+        body_id: rustc_hir::BodyId,
+        _: rustc_span::Span,
+        local_def_id: rustc_hir::def_id::LocalDefId,
+    ) {
+        let to_write = CachedBody::retrieve(self.tcx, local_def_id);
+
+        let dir = &self.target_dir;
+        let path = dir.join(
+            self.tcx
+                .def_path(local_def_id.to_def_id())
+                .to_filename_friendly_no_crate(),
+        );
+
+        if !dir.exists() {
+            std::fs::create_dir(dir).unwrap();
+        }
+
+        encode_to_file(self.tcx, path, &to_write);
+
+        intravisit::walk_fn(
+            self,
+            function_kind,
+            function_declaration,
+            body_id,
+            local_def_id,
+        )
+    }
+}
+
+/// A complete visit over the local crate items, collecting all bodies and
+/// calculating the necessary borrowcheck facts to store for later points-to
+/// analysis.
+///
+/// Ensure this gets called early in the compiler before the unoptimmized mir
+/// bodies are stolen.
+pub fn dump_mir_and_borrowck_facts(tcx: TyCtxt) {
+    let mut vis = DumpingVisitor {
+        tcx,
+        target_dir: intermediate_out_dir(tcx, INTERMEDIATE_ARTIFACT_EXT),
+    };
+    tcx.hir().visit_all_item_likes_in_crate(&mut vis);
+}
+
+const INTERMEDIATE_ARTIFACT_EXT: &str = "bwbf";
+
+/// Get the path where artifacts from this crate would be stored. Unlike
+/// [`TyCtxt::crate_extern_paths`] this function does not crash when supplied
+/// with [`LOCAL_CRATE`].
+pub fn local_or_remote_paths(krate: CrateNum, tcx: TyCtxt, ext: &str) -> Vec<PathBuf> {
+    if krate == LOCAL_CRATE {
+        vec![intermediate_out_dir(tcx, ext)]
+    } else {
+        tcx.crate_extern_paths(krate)
+            .iter()
+            .map(|p| p.with_extension(ext))
+            .collect()
+    }
+}
+
+/// Try to load a [`CachedBody`] for this id.
+pub fn load_body_and_facts(tcx: TyCtxt<'_>, def_id: DefId) -> Result<CachedBody<'_>, String> {
+    let paths = local_or_remote_paths(def_id.krate, tcx, INTERMEDIATE_ARTIFACT_EXT);
+    for path in &paths {
+        let path = path.join(tcx.def_path(def_id).to_filename_friendly_no_crate());
+        if let Ok(data) = decode_from_file(tcx, path) {
+            return Ok(data);
+        };
+    }
+
+    return Err(format!(
+        "No facts for {def_id:?} found at any path tried: {paths:?}"
+    ));
+}
+
+/// Create the name of the file in which to store intermediate artifacts.
+///
+/// HACK(Justus): `TyCtxt::output_filenames` returns a file stem of
+/// `lib<crate_name>-<hash>`, whereas `OutputFiles::with_extension` returns a file
+/// stem of `<crate_name>-<hash>`. I haven't found a clean way to get the same
+/// name in both places, so i just assume that these two will always have this
+/// relation and prepend the `"lib"` here.
+pub fn intermediate_out_dir(tcx: TyCtxt, ext: &str) -> PathBuf {
+    let rustc_out_file = tcx.output_filenames(()).with_extension(ext);
+    let dir = rustc_out_file
+        .parent()
+        .unwrap_or_else(|| panic!("{} has no parent", rustc_out_file.display()));
+    let file = rustc_out_file
+        .file_name()
+        .unwrap_or_else(|| panic!("has no file name"))
+        .to_str()
+        .unwrap_or_else(|| panic!("not utf8"));
+
+    let file = if file.starts_with("lib") {
+        std::borrow::Cow::Borrowed(file)
+    } else {
+        format!("lib{file}").into()
+    };
+
+    dir.join(file.as_ref())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,32 @@
-#![feature(rustc_private)]
+#![feature(rustc_private, box_patterns, min_specialization)]
 
 #[macro_use]
 extern crate tracing;
 #[macro_use]
 extern crate rustc_middle;
-
+extern crate polonius_engine;
+extern crate rustc_borrowck;
+extern crate rustc_const_eval;
 extern crate rustc_data_structures;
 extern crate rustc_driver;
 extern crate rustc_errors;
 extern crate rustc_fluent_macro;
+extern crate rustc_hash;
 extern crate rustc_hir;
 extern crate rustc_interface;
 extern crate rustc_lint;
 extern crate rustc_macros;
 extern crate rustc_monomorphize;
+extern crate rustc_serialize;
 extern crate rustc_session;
 extern crate rustc_smir;
 extern crate rustc_span;
 extern crate rustc_target;
+extern crate rustc_type_ir;
 
 rustc_fluent_macro::fluent_messages! { "../messages.ftl" }
 
+use body_cache::{dump_mir_and_borrowck_facts, is_mir_available, substituted_mir};
 use clap::Parser;
 use reachability::collect_mono_items_from;
 use rustc_hir::ItemKind;
@@ -29,9 +35,11 @@ use rustc_middle::{
     ty::{self, TyCtxt},
 };
 use rustc_plugin::{CrateFilter, RustcPlugin, RustcPluginArgs, Utf8Path};
+use rustc_utils::mir::borrowck_facts;
 use serde::{Deserialize, Serialize};
 use std::{borrow::Cow, env, fs, process::Command};
 
+mod body_cache;
 mod reachability;
 
 pub struct PeircePlugin;
@@ -40,6 +48,32 @@ pub struct PeircePlugin;
 pub struct PeircePluginArgs {
     #[clap(last = true)]
     cargo_args: Vec<String>,
+}
+
+fn get_default_rustc_target() -> Result<String, String> {
+    const RUSTC_COMMAND: &str = "rustc";
+    const HOST_PREFIX: &str = "host: ";
+    const VERBOSE_VERSION_ARG: &str = "-vV";
+
+    let output = Command::new(RUSTC_COMMAND)
+        .arg(VERBOSE_VERSION_ARG)
+        .output()
+        .map_err(|_| "cannot get default rustc target")?;
+
+    let stdout = String::from_utf8(output.stdout).map_err(|_| "cannot parse stdout")?;
+
+    let mut target = String::from("");
+    for part in stdout.split("\n") {
+        if part.starts_with(HOST_PREFIX) {
+            target = part.chars().skip(HOST_PREFIX.len()).collect();
+        }
+    }
+
+    if target.len() == 0 {
+        return Err("cannot find target".into());
+    }
+
+    Ok(target)
 }
 
 impl RustcPlugin for PeircePlugin {
@@ -60,48 +94,78 @@ impl RustcPlugin for PeircePlugin {
     }
 
     fn modify_cargo(&self, cargo: &mut Command, _args: &Self::Args) {
-        // Find the default target triplet.
-        let output = Command::new("rustc")
-            .arg("-vV")
-            .output()
-            .expect("Cannot get default rustc target");
-        let stdout = String::from_utf8(output.stdout).expect("Cannot parse stdout");
+        const CARGO_BUILD_STD_ARG: &str = "-Zbuild-std=std,core,alloc,proc_macro";
 
-        let mut target = String::from("");
-        for part in stdout.split("\n") {
-            if part.starts_with("host: ") {
-                target = part.chars().skip("host: ".len()).collect();
-            }
-        }
-        if target.len() == 0 {
-            panic!("Bad output");
-        }
-
-        // Add -Zalways-encode-mir to RUSTFLAGS.
-        let mut old_rustflags = String::from("");
-        for (key, val) in cargo.get_envs() {
-            if key == "RUSTFLAGS" {
-                if let Some(val) = val {
-                    old_rustflags = format!("{}", val.to_str().unwrap());
-                }
-            }
-        }
-        cargo.env(
-            "RUSTFLAGS",
-            format!("-Zalways-encode-mir {}", old_rustflags),
-        );
-        cargo.arg("-Zbuild-std=std,core,alloc,proc_macro");
-        cargo.arg(format!("--target={}", target));
+        cargo.arg(CARGO_BUILD_STD_ARG);
+        cargo.arg(format!("--target={}", get_default_rustc_target().unwrap()));
     }
 
     fn run(
         self,
-        compiler_args: Vec<String>,
+        mut compiler_args: Vec<String>,
         plugin_args: Self::Args,
     ) -> rustc_interface::interface::Result<()> {
-        let mut callbacks = PeircePluginCallbacks { args: plugin_args };
-        let compiler = rustc_driver::RunCompiler::new(&compiler_args, &mut callbacks);
-        compiler.run()
+        const RUSTC_ALWAYS_ENCODE_MIR_ARG: &str = "-Zalways-encode-mir";
+
+        compiler_args.push(RUSTC_ALWAYS_ENCODE_MIR_ARG.into());
+        let mut callbacks = match how_to_handle_this_crate(&mut compiler_args) {
+            CrateHandling::JustCompile => {
+                Box::new(NoopCallbacks) as Box<dyn rustc_driver::Callbacks + Send>
+            }
+            CrateHandling::CompileAndDump => Box::new(DumpOnlyCallbacks),
+            CrateHandling::Analyze => Box::new(PeircePluginCallbacks { args: plugin_args }),
+        };
+        rustc_driver::RunCompiler::new(&compiler_args, callbacks.as_mut()).run()
+    }
+}
+
+enum CrateHandling {
+    JustCompile,
+    CompileAndDump,
+    Analyze,
+}
+
+fn how_to_handle_this_crate(compiler_args: &mut Vec<String>) -> CrateHandling {
+    const BUILD_SCRIPT_CRATE_NAME: &str = "build_script_build";
+    const CARGO_PRIMARY_PACKAGE_ENV_VAR: &str = "CARGO_PRIMARY_PACKAGE";
+
+    let crate_name = compiler_args
+        .iter()
+        .enumerate()
+        .find_map(|(i, s)| (s == "--crate-name").then_some(i))
+        .and_then(|i| compiler_args.get(i + 1))
+        .cloned();
+
+    match &crate_name {
+        Some(krate) if krate == BUILD_SCRIPT_CRATE_NAME => CrateHandling::JustCompile,
+        _ if std::env::var(CARGO_PRIMARY_PACKAGE_ENV_VAR).is_ok() => CrateHandling::Analyze,
+        Some(_) => CrateHandling::CompileAndDump,
+        _ => CrateHandling::JustCompile,
+    }
+}
+
+struct NoopCallbacks;
+
+impl rustc_driver::Callbacks for NoopCallbacks {}
+
+struct DumpOnlyCallbacks;
+
+impl rustc_driver::Callbacks for DumpOnlyCallbacks {
+    fn config(&mut self, config: &mut rustc_interface::Config) {
+        // Configure rustc to ensure `get_body_with_borrowck_facts` will work.
+        borrowck_facts::enable_mir_simplification();
+        config.override_queries = Some(borrowck_facts::override_queries);
+    }
+
+    fn after_expansion<'tcx>(
+        &mut self,
+        _compiler: &rustc_interface::interface::Compiler,
+        queries: &'tcx rustc_interface::Queries<'tcx>,
+    ) -> rustc_driver::Compilation {
+        queries.global_ctxt().unwrap().enter(|tcx| {
+            dump_mir_and_borrowck_facts(tcx);
+        });
+        rustc_driver::Compilation::Continue
     }
 }
 
@@ -110,6 +174,23 @@ struct PeircePluginCallbacks {
 }
 
 impl rustc_driver::Callbacks for PeircePluginCallbacks {
+    fn config(&mut self, config: &mut rustc_interface::Config) {
+        // Configure rustc to ensure `get_body_with_borrowck_facts` will work.
+        borrowck_facts::enable_mir_simplification();
+        config.override_queries = Some(borrowck_facts::override_queries);
+    }
+
+    fn after_expansion<'tcx>(
+        &mut self,
+        _compiler: &rustc_interface::interface::Compiler,
+        queries: &'tcx rustc_interface::Queries<'tcx>,
+    ) -> rustc_driver::Compilation {
+        queries.global_ctxt().unwrap().enter(|tcx| {
+            dump_mir_and_borrowck_facts(tcx);
+        });
+        rustc_driver::Compilation::Continue
+    }
+
     fn after_analysis<'tcx>(
         &mut self,
         _compiler: &rustc_interface::interface::Compiler,
@@ -118,25 +199,34 @@ impl rustc_driver::Callbacks for PeircePluginCallbacks {
         queries
             .global_ctxt()
             .unwrap()
-            .enter(|tcx| print_all_items(tcx, &self.args))
+            .enter(|tcx| dump_all_items(tcx, &self.args))
     }
 }
 
 // Analysis callback.
-fn print_all_items(tcx: TyCtxt, _args: &PeircePluginArgs) -> rustc_driver::Compilation {
+fn dump_all_items(tcx: TyCtxt, _args: &PeircePluginArgs) -> rustc_driver::Compilation {
     tcx.hir().items().for_each(|item_id| {
         let hir = tcx.hir();
         let item = hir.item(item_id);
         let def_id = item.owner_id.to_def_id();
+
         if let ItemKind::Fn(..) = &item.kind {
             let instance =
                 ty::Instance::new(def_id, ty::GenericArgs::identity_for_item(tcx, def_id));
+            let (items, usage_map) = collect_mono_items_from(tcx, MonoItem::Fn(instance));
+            for item in items.into_iter() {
+                match item {
+                    MonoItem::Fn(instance) => {
+                        assert!(is_mir_available(instance.def_id(), tcx));
+                        let _body = substituted_mir(&instance, tcx);
+                    }
+                    MonoItem::Static(_def_id) => {}
+                    MonoItem::GlobalAsm(_item_id) => {}
+                }
+            }
             fs::write(
                 format!("{:?}.peirce.json", item_id.owner_id),
-                serde_json::to_string_pretty(
-                    &collect_mono_items_from(tcx, MonoItem::Fn(instance)).1,
-                )
-                .unwrap(),
+                serde_json::to_string_pretty(&usage_map).unwrap(),
             )
             .unwrap();
         }
